fa7c88483b8977d82ebccac1e2458462c5f6c824
NIKHILESHM <nikhileshm9@gmail.com>
2017-06-02 17:27:35 +0530

    Matlab code for static images

--- Camera Calibration Toolbox for Matlab - Arena/calib_example/graph.m
+++
@@ -1,8 +0,0 @@
-figure();
-hold on;
-plot(KK_PX, KK_PY, 'g+');
-plot([100 110 110 100 100],[63 63 69 69 63], 'r-');
-plot([100 110 110 100],[63 63 69 69], 'b*');
-title('Four Corners of Cube')
-xlabel('Xw(mm)')
-ylabel('Yw(mm)')
